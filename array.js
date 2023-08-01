113e21d2ed407c5ccaf4a8da6ca04ecb2951708e
Leila.bwt <leila.bwt@gmail.com>
2023-07-31 21:29:28 -0300

    Exemplos de importação de arquivos

--- array.js
+++ array.js
@@ -1,3 +1,21 @@
 const arrayNumeros = [1, 2, 4, 4, 6, 7, 1];
 
-module.exports = array;
+const arrayLetras = ['a', 'a', 's', 'c'];
+
+// importando objetos
+
+module.exports = {
+    arrayNumeros,                // se o nome da propriedade for igual ao nome do valor
+    arrayLetras: arrayLetras,   // passa somente o um nome
+}; 
+
+// isso é igual a:
+
+/* const objeto = {
+*   arrayNumeros: arrayNumeros,
+    arrayletras: arrayLetras,
+*};
+
+module.exports = objetos;
+*/
+
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
  const { uniq } = require('lodash');// importa a função
- const array = require('./array');// importa o arquivo
+ const { arrayNumeros, arrayLetras } = require('./array');
 
- console.log(array);
- const arrayUnico = uniq(array);
+ console.log(arrayNumeros);
+ console.log(arrayLetras); // consegue pegar separado na importação devido a desestruturação
 
- console.log(arrayUnico);
+
+ const arrayNumeroUnico = uniq(arrayNumeros);
+ const arrayLetraUnica = uniq(arrayLetras);
+
+ console.log(arrayNumeroUnico);
+ console.log(arrayLetraUnica);
